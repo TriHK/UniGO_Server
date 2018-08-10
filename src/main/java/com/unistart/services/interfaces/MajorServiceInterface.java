183b05a1273e774e13dca0875b834d8a8ec58b54
Nam Thanh Le <lethanhnam2882@gmail.com>
2018-08-10 16:16:52 +0700

    Add get popular major api

--- src/main/java/com/unistart/services/interfaces/MajorServiceInterface.java
+++ src/main/java/com/unistart/services/interfaces/MajorServiceInterface.java
@@ -8,18 +8,27 @@ import com.unistart.entities.University;
 
 public interface MajorServiceInterface {
 
-	List<Major> listAllMajorName();
+    List<Major> listAllMajorName();
+
+    List<Major> listPopularMajor();
+
     Major getMajorById(int id);
-	boolean saveMajorUniversity(Major major, University uni);
-	boolean changeActive(int majorUniId, boolean isActive);
-	List<MajorUniversity> getUniverityWithMajor(University uni);
-	MajorUniversity findByMajorIdAndUniId(int majorId, int uniId);
 
-	boolean saveMajorUniDetail(int id, double year, String des, String requirement, String pros);
+    boolean saveMajorUniversity(Major major, University uni);
 
-	List<University> getByMajor(int majorId);
+    boolean changeActive(int majorUniId, boolean isActive);
 
-	List<MajorUniversity> getListId();
-	List<MajorUniversity> getByUniversityId(int uniId);
-	List<MajorUniversity> getForTag(int uniId);
+    List<MajorUniversity> getUniverityWithMajor(University uni);
+
+    MajorUniversity findByMajorIdAndUniId(int majorId, int uniId);
+
+    boolean saveMajorUniDetail(int id, double year, String des, String requirement, String pros);
+
+    List<University> getByMajor(int majorId);
+
+    List<MajorUniversity> getListId();
+
+    List<MajorUniversity> getByUniversityId(int uniId);
+
+    List<MajorUniversity> getForTag(int uniId);
 }
